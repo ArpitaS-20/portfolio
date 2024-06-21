973acee811282f84a3b46112fd0124435da534ff
ArpitaS-20 <122557922+ArpitaS-20@users.noreply.github.com>
2024-06-21 22:50:40 +0530

    Add files via upload

---
+++ portfolio_website/scripts.js
@@ -0,0 +1,3 @@
+// JavaScript code can be used to enhance the functionality of your portfolio
+// For this simple example, no JavaScript is required, but you can add interactivity here if needed
+console.log("Portfolio loaded successfully!");
