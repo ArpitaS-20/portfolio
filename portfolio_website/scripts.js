1b35b0aeace006fa155ae6db2745643f2dc81ce0
ArpitaS-20 <122557922+ArpitaS-20@users.noreply.github.com>
2024-06-22 11:25:58 +0530

    Delete portfolio_website directory

--- portfolio_website/scripts.js
+++
@@ -1,3 +0,0 @@
-// JavaScript code can be used to enhance the functionality of your portfolio
-// For this simple example, no JavaScript is required, but you can add interactivity here if needed
-console.log("Portfolio loaded successfully!");
